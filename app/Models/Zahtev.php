91ff8b7fe21293319f985e0af122b6a58b9d4ee0
Ana Boskovic <anaboskovicmis@gmail.com>
2023-12-08 09:18:45 +0100

    Zahtev model

---
+++ app/Models/Zahtev.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Zahtev extends Model
+{
+    use HasFactory;
+    protected $table = 'zahtevi';
+    
+    protected $fillable = [
+        'datumUsluge',
+        'user_id',
+        'usluga_id',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function usluga()
+    {
+        return $this->belongsTo(Usluga::class);
+    }
+}
+
