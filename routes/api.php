77c841ac646e00597922cc2ea9d4784474acaab2
Bogdana Bradic <bogdanabradic8@gmail.com>
2023-12-13 19:53:38 +0100

    route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//login i register
+
+Route::post('/login', 'App\Http\Controllers\UserController@login');
+Route::post('/register', 'App\Http\Controllers\UserController@register');
+
+//sanctum middleware
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', 'App\Http\Controllers\UserController@logout');
+    Route::post('/changePassword', 'App\Http\Controllers\UserController@changePassword');
+    Route::resource('/kategorije', 'App\Http\Controllers\KategorijaController');
+    Route::get('/zahtevi', 'App\Http\Controllers\ZahtevController@index');
+    Route::get('/zahtevi/{id}', 'App\Http\Controllers\ZahtevController@show');
+    Route::post('/zahtevi', 'App\Http\Controllers\ZahtevController@store');
+    Route::put('/zahtevi/{id}', 'App\Http\Controllers\ZahtevController@update');
+    Route::delete('/zahtevi/{id}', 'App\Http\Controllers\ZahtevController@destroy');
+    Route::get('/usluge', 'App\Http\Controllers\UslugaController@index');
+    Route::get('/usluge/{id}', 'App\Http\Controllers\UslugaController@show');
+    Route::post('/usluge', 'App\Http\Controllers\UslugaController@store');
+    Route::put('/usluge/{id}', 'App\Http\Controllers\UslugaController@update');
+    Route::delete('/usluge/{id}', 'App\Http\Controllers\UslugaController@destroy');
+    Route::get('/zahtevi-paginacija', 'App\Http\Controllers\ZahtevController@paginacija');
+    Route::get('/zahtevi-korisnika/{id}', 'App\Http\Controllers\ZahtevController@zahteviKorisnika');
 });
