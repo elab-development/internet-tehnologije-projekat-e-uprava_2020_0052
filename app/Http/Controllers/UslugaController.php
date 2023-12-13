84d5292d5d1f21bfb6a90e677c315844e060e9f8
Ana Boskovic <anaboskovicmis@gmail.com>
2023-12-13 19:48:25 +0100

    usluga i zahtev controller

---
+++ app/Http/Controllers/UslugaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UslugaResurs;
+use App\Models\Usluga;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UslugaController extends BaseController
+{
+    public function index()
+    {
+        $usluge = Usluga::all();
+        return $this->success(UslugaResurs::collection($usluge), 'Uspesno prikazane usluge.');
+    }
+
+    public function show($id)
+    {
+        $usluga = Usluga::find($id);
+        if ($usluga) {
+            return $this->success(new UslugaResurs($usluga), 'Uspesno prikazana usluga.');
+        } else {
+            return $this->error('Usluga ne postoji.', null, 404);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        //validate
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'nazivUsluge' => 'required',
+            'opisUsluge' => 'required',
+            'kategorija_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Neispravni podaci.', $validator->errors());
+        }
+
+        $usluga = new Usluga();
+        $usluga->nazivUsluge = $request->nazivUsluge;
+        $usluga->opisUsluge = $request->opisUsluge;
+        $usluga->kategorija_id = $request->kategorija_id;
+        $usluga->save();
+        return $this->success(new UslugaResurs($usluga), 'Uspesno kreirana usluga.');
+    }
+
+    public function update(Request $request, $id)
+    {
+        //validate
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'nazivUsluge' => 'required',
+            'opisUsluge' => 'required',
+            'kategorija_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Neispravni podaci.', $validator->errors());
+        }
+
+        $usluga = Usluga::find($id);
+        if ($usluga) {
+            $usluga->nazivUsluge = $request->nazivUsluge;
+            $usluga->opisUsluge = $request->opisUsluge;
+            $usluga->kategorija_id = $request->kategorija_id;
+            $usluga->save();
+            return $this->success(new UslugaResurs($usluga), 'Uspesno izmenjena usluga.');
+        } else {
+            return $this->error('Usluga ne postoji.', null, 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $usluga = Usluga::find($id);
+        if ($usluga) {
+            $usluga->delete();
+            return $this->success(null, 'Uspesno obrisana usluga.');
+        } else {
+            return $this->error('Usluga ne postoji.', null, 404);
+        }
+    }
+}
---
+++ app/Http/Controllers/ZahtevController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ZahtevResurs;
+use App\Models\Zahtev;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ZahtevController extends BaseController
+{
+    public function index()
+    {
+        $zahtevi = Zahtev::all();
+        return $this->success(ZahtevResurs::collection($zahtevi), 'Uspesno prikazani zahtevi.');
+    }
+
+    public function show($id)
+    {
+        $zahtev = Zahtev::find($id);
+        if ($zahtev) {
+            return $this->success(new ZahtevResurs($zahtev), 'Uspesno prikazan zahtev.');
+        } else {
+            return $this->error('Zahtev ne postoji.', null, 404);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        //validate
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'datumUsluge' => 'required',
+            'user_id' => 'required',
+            'usluga_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Neispravni podaci.', $validator->errors());
+        }
+
+        $zahtev = new Zahtev();
+        $zahtev->datumUsluge = $request->datumUsluge;
+        $zahtev->user_id = $request->user_id;
+        $zahtev->usluga_id = $request->usluga_id;
+        $zahtev->save();
+        return $this->success(new ZahtevResurs($zahtev), 'Uspesno kreiran zahtev.');
+    }
+
+    public function update(Request $request, $id)
+    {
+        //validate
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'datumUsluge' => 'required',
+            'user_id' => 'required',
+            'usluga_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error('Neispravni podaci.', $validator->errors());
+        }
+
+        $zahtev = Zahtev::find($id);
+        if ($zahtev) {
+            $zahtev->datumUsluge = $request->datumUsluge;
+            $zahtev->user_id = $request->user_id;
+            $zahtev->usluga_id = $request->usluga_id;
+            $zahtev->save();
+            return $this->success(new ZahtevResurs($zahtev), 'Uspesno izmenjen zahtev.');
+        } else {
+            return $this->error('Zahtev ne postoji.', null, 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $zahtev = Zahtev::find($id);
+        if ($zahtev) {
+            $zahtev->delete();
+            return $this->success(null, 'Uspesno obrisan zahtev.');
+        } else {
+            return $this->error('Zahtev ne postoji.', null, 404);
+        }
+    }
+
+    public function zahteviKorisnika($id)
+    {
+        $zahtevi = Zahtev::where('user_id', $id)->get();
+        return $this->success(ZahtevResurs::collection($zahtevi), 'Uspesno prikazani zahtevi.');
+    }
+
+    public function paginacija(Request $request)
+    {
+        $pageSize = min($request->get('page_size'), 100);
+        $zahtevi = Zahtev::paginate($pageSize);
+        return $this->success(ZahtevResurs::collection($zahtevi), 'Uspesno prikazani zahtevi.');
+    }
+}
